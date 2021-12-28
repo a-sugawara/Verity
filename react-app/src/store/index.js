ebdbe465f7383907138042efd5d3beb4e16b76e9
Alfredo Sugawara <nicora221@gmail.com>
2021-12-28 11:02:47 -0500

    profile page css

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import articles from './articles'
+import profile from './profile'
 
 const rootReducer = combineReducers({
   session,
   articles,
+  profile,
 
 });
 
