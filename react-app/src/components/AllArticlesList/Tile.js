7ed6edd6d6c80c63b40acedb2fca0965e47fa76a
Alfredo Sugawara <nicora221@gmail.com>
2022-03-09 23:42:50 -0500

    Chore(optimizing number of calculations)

--- react-app/src/components/AllArticlesList/Tile.js
+++ react-app/src/components/AllArticlesList/Tile.js
@@ -3,20 +3,18 @@ import{ NavLink } from 'react-router-dom'
 const Tile = ({article, idx}) =>{
     let avg = article.ratings.sum/article.ratings.len || 0
 
-
-
     return <div key={idx} className={`list-card-container`}>
                         {avg>75? <div className={`list-sidebar-green`}></div>:null}
-                        {avg>0 && (article.ratings.len >0?article.ratings.sum/article.ratings.len:0 )<50? <div className={`list-sidebar-red`} ></div>:null}
+                        {avg>0 && avg<50? <div className={`list-sidebar-red`} ></div>:null}
                         {avg===0? <div className={`list-sidebar`} ></div>:null}
-                        {avg>=50 && (article.ratings.len >0?article.ratings.sum/article.ratings.len:0 )<=75? <div className={`list-sidebar-yellow`} ></div>:null}
+                        {avg>=50 && avg<=75? <div className={`list-sidebar-yellow`} ></div>:null}
                     <div className="list-title">
                         <NavLink to={`/articles/${article.id}`}>
                             <div className="titlebtn">{article.title}</div>
                         </NavLink>
                     </div>
                     <div className="list-description">
-                        Posted by: {article.username}<br/>{article.ratings.len >0?Math.round(article.ratings.sum/article.ratings.len):0}% accuracy rating<br/> {article.comments} comments
+                        Posted by: {article.username}<br/>{Math.round(avg)}% accuracy rating<br/> {article.comments} comments
                         <div className="navbtn">
                             <a target="_blank" without rel="noreferrer" href={article.description}>Source</a>
                         </div>
@@ -29,14 +27,14 @@ const Tile = ({article, idx}) =>{
                     <div className="v-score">
                         <div className="list-badges">
 
-                            {(article.ratings.len >0?article.ratings.sum/article.ratings.len:0 )>75? <img alt="chckmark" className="verified-img" src="https://cdn.discordapp.com/attachments/920285009099751524/921974219733082173/Verified.png"/>:null}
-                            {(article.ratings.len >0?article.ratings.sum/article.ratings.len:0 )>0 && (article.ratings.len >0?article.ratings.sum/article.ratings.len:0 )<50? <img alt="xmrk" className="verified-img" src="https://cdn.discordapp.com/attachments/920285009099751524/922093439854731274/UnVerifiedlogo.png"/>:null}
+                            {avg>75? <img alt="chckmark" className="verified-img" src="https://cdn.discordapp.com/attachments/920285009099751524/921974219733082173/Verified.png"/>:null}
+                            {avg>0 && avg<50? <img alt="xmrk" className="verified-img" src="https://cdn.discordapp.com/attachments/920285009099751524/922093439854731274/UnVerifiedlogo.png"/>:null}
                         </div>
                         <br/>
-                        {(article.ratings.len >0?article.ratings.sum/article.ratings.len:0 )===0? <span className='weak-grey'>This has Not Been voted on yet</span>:null}
-                        {(article.ratings.len >0?article.ratings.sum/article.ratings.len:0 )>75? <span className='weak-grey'>This has been deemed an article of truth</span>:null}
-                        {(article.ratings.len >0?article.ratings.sum/article.ratings.len:0 )>0 && (article.ratings.len >0?article.ratings.sum/article.ratings.len:0 )<50? <span className='weak-grey'>This has been deemed an article of untruth</span>:null}
-                        {(article.ratings.len >0?article.ratings.sum/article.ratings.len:0 )>=50 && (article.ratings.len >0?article.ratings.sum/article.ratings.len:0 )<=75? <span className='weak-grey'>This is still up for debate</span>:null}
+                        {avg===0? <span className='weak-grey'>This has Not Been voted on yet</span>:null}
+                        {avg>75? <span className='weak-grey'>This has been deemed an article of truth</span>:null}
+                        {avg>0 && avg<50? <span className='weak-grey'>This has been deemed an article of untruth</span>:null}
+                        {avg>=50 && avg<=75? <span className='weak-grey'>This is still up for debate</span>:null}
 
                     </div>
                 </div>
